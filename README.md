92114ed980f06b34b3e9c148a4748f71c4abfff5
heejung1180 <heejung1180@outlook.com>
2025-01-03 17:47:00 -0800

    slideshow functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # single-page-design-portfolio
 
 Tailwind Play CDN https://tailwindcss.com/docs/installation/play-cdn
+
+How to Create a Slideshow https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow
--- script.js
+++ script.js
@@ -48,3 +48,61 @@ tailwind.config = {
     },
   },
 };
+
+let slideIndexLeft = 2;
+showSlidesLeft(slideIndexLeft);
+let slideIndex = 3;
+showSlides(slideIndex);
+let slideIndexRight = 4;
+showSlidesRight(slideIndexRight);
+
+function plusSlides(n) {
+  showSlidesLeft((slideIndexLeft += n));
+  showSlides((slideIndex += n));
+  showSlidesRight((slideIndexRight += n));
+}
+
+function showSlidesLeft(n) {
+  let i;
+  let slidesLeft = document.getElementsByClassName("mySlidesLeft");
+  if (n > slidesLeft.length) {
+    slideIndexLeft = 1;
+  }
+  if (n < 1) {
+    slideIndexLeft = slidesLeft.length;
+  }
+  for (i = 0; i < slidesLeft.length; i++) {
+    slidesLeft[i].style.display = "none";
+  }
+  slidesLeft[slideIndexLeft - 1].style.display = "block";
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+function showSlidesRight(n) {
+  let i;
+  let slidesRight = document.getElementsByClassName("mySlidesRight");
+  if (n > slidesRight.length) {
+    slideIndexRight = 1;
+  }
+  if (n < 1) {
+    slideIndexRight = slidesRight.length;
+  }
+  for (i = 0; i < slidesRight.length; i++) {
+    slidesRight[i].style.display = "none";
+  }
+  slidesRight[slideIndexRight - 1].style.display = "block";
+}
